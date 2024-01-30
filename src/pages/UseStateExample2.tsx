330f6ce0567f79db8bc52c8e32c091aa98246a27
DarkAsfu <ashraful.islam.asfu@gmail.com>
2024-01-30 14:51:23 +0600

    Refactoring Change Event Functions

--- src/pages/UseStateExample2.tsx
+++ src/pages/UseStateExample2.tsx
@@ -4,15 +4,28 @@ const UseStateExample2 = () => {
     // const [name, setName] = useState("")
     // const [email, setemail] = useState("")
     // console.log(name, email);
-    const [user, setUser] = useState({name: '', email: ''})
-    const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) =>{
+    const [user, setUser] = useState({ name: '', email: '' })
+    const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log(user);
     }
+    const setHandleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        const name = e.target.name;
+        setUser({ ...user, [name]: e.target.value })
+    }
+    /* DRY => Don't repeat yourseft */
     return (
         <form onSubmit={handleSubmit}>
-            <input onChange={(e) => setUser({...user, name: e.target.value})} className='border' placeholder='your name' type="text" name="name" />
-            <input onChange={(e) => setUser({...user, email: e.target.value})} className='border' placeholder='your email' type="eamil" name="email" />
+            <input onChange={setHandleChange}
+                className='border'
+                placeholder='your name'
+                type="text"
+                name="name" />
+            <input onChange={setHandleChange}
+                className='border'
+                placeholder='your email'
+                type="eamil"
+                name="email" />
             <button type="submit">Submit</button>
         </form>
     );
