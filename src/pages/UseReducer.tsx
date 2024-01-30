3b1b866387d6789fd08a7269d296014c201612ee
DarkAsfu <ashraful.islam.asfu@gmail.com>
2024-01-30 15:31:50 +0600

    payload

--- src/pages/UseReducer.tsx
+++ src/pages/UseReducer.tsx
@@ -1,18 +1,23 @@
 import React, { useReducer, useState } from 'react';
 
 const initialState = { count: 0 };
-const reducer = (currectState, action) => {
+const reducer = (currentState, action) => {
+    console.log("CurrentState => ", currentState);
+    console.log("Action => ", action);
     switch (action.type) {
         case "increment":
-            return { count: currectState.count + 1 }
+            return { count: currentState.count + 1 }
             break;
 
         case "decrement":
-            return { count: currectState.count - 1 }
+            return { count: currentState.count - 1 }
+            break;
+        case "incrementBySetAmount":
+            return { count: currentState.count + action.payload }
             break;
 
         default:
-            return currectState;
+            return currentState;
     }
 }
 const UseReducer = () => {
@@ -20,9 +25,10 @@ const UseReducer = () => {
     return (
         <div>
             <h1>{state.count}</h1>
-            <button onClick={() => dispatch({type: 'increment'})} className='btn bg-pink-300 text-white p-2 rounded-md'>Increment</button>
-            <button onClick={() => dispatch({type: 'decrement'})} className='btn bg-black ml-3 text-white p-2 rounded-md'>Decrement</button>
-            
+            <button onClick={() => dispatch({ type: 'increment' })} className='btn bg-pink-300 text-white p-2 rounded-md'>Increment</button>
+            <button onClick={() => dispatch({ type: 'decrement' })} className='btn bg-black ml-3 text-white p-2 rounded-md'>Decrement</button>
+            <button onClick={() => dispatch({ type: 'incrementBySetAmount', payload: 10 })} className='btn bg-yellow-400 ml-3 text-white p-2 rounded-md'>Increment By 3</button>
+
         </div>
     );
 };
